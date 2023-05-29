71debf81f0e332d597884a3121ea3eb1d2fd277b
Arthur Shutov <ridpfrep@yandex.ru>
2023-05-29 23:24:31 +0300

    up

--- backend/api/v1/serializers.py
+++ backend/api/v1/serializers.py
@@ -1,11 +1,15 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
+from rest_framework.validators import UniqueValidator
 
 User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
     class Meta:
         fields = (
             "username",
@@ -15,8 +19,21 @@ class UserSerializer(serializers.ModelSerializer):
         )
         model = User
 
+        extra_kwargs = {
+            "username": {
+                "validators": [UniqueValidator(queryset=User.objects.all())]
+            },
+            "password": {
+                "write_only": True
+            }
+        }
     def validate_username(self, username):
         if username.lower() == "me":
             raise serializers.ValidationError("Использовать имя me запрещено")
         return username
 
+
+class TokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ("email", "password")
+        model = User
