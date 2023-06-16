66b477111b24a1282b1684eea673c7cc42c7ebf3
Arthur Shutov <ridpfrep@yandex.ru>
2023-06-16 22:36:31 +0300

    final

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,4 @@
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.password_validation import validate_password
-from django.core.exceptions import ValidationError
 from django.db import models
 
 
